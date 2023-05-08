4aa1118fcd0dff4f23cbc498578cfd3a459f033e
Austin Howard <austinhoward@Austins-MacBook-Pro.local>
2023-05-08 16:41:06 -0500

    Addng my current deconstructed branch for diagnose purposes

--- src/main/java/com/checkin/controller/CheckInController.java
+++ src/main/java/com/checkin/controller/CheckInController.java
@@ -2,65 +2,45 @@ package com.checkin.controller;
 
 import com.checkin.helper.CheckInHelper;
 import com.pi4j.context.Context;
-import com.pi4j.io.i2c.I2CConfig;
+//import com.pi4j.io.i2c.I2CConfig;
 import com.pi4j.io.pwm.Pwm;
 import com.pi4j.io.spi.SpiConfig;
+import io.micronaut.core.annotation.Introspected;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import jakarta.inject.Inject;
 import jakarta.inject.Named;
+import io.micronaut.core.annotation.Introspected;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.*;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
 @Controller("/checkin")
 public class CheckInController {
     private final CheckInHelper helper;
 
-    public CheckInController(@Named("rfid") SpiConfig spi, @Named("lcd") I2CConfig i2CConfig,
-                             @Named("red-pwm") Pwm red, @Named("blue-pwm") Pwm blue, @Named("green-pwm") Pwm green,
-                             Context pi4jContext){
-        this.helper = new CheckInHelper(spi, i2CConfig, red, blue, green, pi4jContext);
+
+    public CheckInController(@Named("red-pwm") Pwm red, @Named("blue-pwm") Pwm blue, @Named("green-pwm") Pwm green,
+                             Context pi4jContext) {
+
+        this.helper = new CheckInHelper(red, blue, green, pi4jContext);
     }
 
-    @Get("/init")
-    public void init(){
-        try {
-            helper.mainLoop();
+    //     Say "in" for checking in, or "out" for checking out, for now
+    @Get("/led/{status}")
+    public String checkIn(String status) {
+        String message;
+        if (status.toString() == "in"){
+            message = helper.turnOnLight(true);
         }
-        catch (InterruptedException e){
-            System.out.println(e.toString());
+        else if (status.toString() == "out"){
+            message = helper.turnOnLight(false);
         }
+        else {
+            message = "Error checking you in";
+        }
+        return message;
     }
-
-//    @Get("/getAllCards")
-//    public String getAllCards(){
-//        return helper.getName();
-//    }
-//
-//    @Get("/getCheckedIn")
-//    public List<String> getCheckedIn(){
-//        return helper.getCheckedIn();
-//    }
-//
-//    @Get("/getCheckedOut")
-//    public List<String> getCheckedOut(){
-//        return helper.getCheckedOut();
-//    }
-//
-//
-//    @Get("/removeCard")
-//    public boolean removeCard(String name){
-//        return helper.removeCard(name);
-//    }
-
-    @Get("/newCard/{value}")
-    public void addNewCard(String value){
-        try {
-            helper.addNewCard(value);
-        }
-        catch (InterruptedException e){
-            System.out.println(e.toString());
-        }
-    }
-
 }
+
--- src/main/java/com/checkin/helper/CheckInHelper.java
+++ src/main/java/com/checkin/helper/CheckInHelper.java
@@ -1,102 +1,38 @@
 package com.checkin.helper;
 
-import com.opensourcewithslu.inputdevices.RFidHelper;
-import com.opensourcewithslu.outputdevices.LCD1602Helper;
 import com.opensourcewithslu.outputdevices.RGBLEDHelper;
 import com.pi4j.context.Context;
-import com.pi4j.io.i2c.I2CConfig;
 import com.pi4j.io.pwm.Pwm;
-import com.pi4j.io.spi.SpiConfig;
 import io.micronaut.context.annotation.Prototype;
-import java.util.HashMap;
 
 
 @Prototype
 public class CheckInHelper {
-    private final RFidHelper rfid;
+    //    private final RFidHelper rfid;
     private final RGBLEDHelper rgb;
-    private final LCD1602Helper lcd;
+//    private final LCD1602Helper lcd;
 
-    private HashMap<String, Boolean> names;
+//    private HashMap<String, Boolean> names;
+
+
 
     int[] red = new int[] {100, 0, 0};
     int[] green = new int[] {0, 100, 0};
 
-    public CheckInHelper(SpiConfig spi, I2CConfig i2CConfig,
-                         Pwm red, Pwm blue, Pwm green,
+    public CheckInHelper(Pwm red, Pwm blue, Pwm green,
                          Context pi4jContext){
 
-        this.rfid = new RFidHelper(spi, 25, pi4jContext);
         this.rgb = new RGBLEDHelper(red, blue, green);
-        this.lcd = new LCD1602Helper(i2CConfig, pi4jContext);
-
-        // temporary hashmap in place of h2 db
-        this.names = new HashMap<String, Boolean>();
-        names.put("Traison", false);
-        names.put("Greih", false);
-        names.put("Austin", false);
-        names.put("Sinuo", false);
     }
 
-    public void mainLoop()
-        throws InterruptedException{
-
-        lcd.writeText("Welcome to OSS");
-        Thread.sleep(3000);
-        lcd.clearDisplay();
-
-        while(true) {
-            lcd.writeText("Please scan your card");
-            // if valid read
-            try{
-                Object name = rfid.readFromCard();
-                boolean inMap = this.names.containsKey(name.toString());
-                // if in hashmap
-                if (inMap){
-                    rgb.setColor(green);
-                    boolean checkedIn = this.names.get(name.toString());
-                    // if already checked in
-                    if (checkedIn) {
-                        this.names.put(name.toString(), false);
-                        lcd.writeText("Goodbye " + name.toString());
-                    }
-                    // if not checked in
-                    else{
-                        this.names.put(name.toString(), true);
-                        lcd.writeText("Hello " + name.toString());
-                    }
-                }
-                // if not in hashmap
-                else{
-                    rgb.setColor(red);
-                    lcd.writeText("User: " + name.toString() + " not recognized.");
-                    Thread.sleep(1000);
-                    lcd.writeText("Please request new user access");
-                }
-            }
-            catch (NullPointerException e){
-                rgb.setColor(red);
-                lcd.writeText("Invalid card read. Please try again");
-            }
-            rfid.resetScanner();
-            Thread.sleep(2000);
-            rgb.ledOff();
+    public String turnOnLight(Boolean checkedIn) {
+        if (checkedIn) {
+            rgb.setColor(green);
+            return "Welcome!";
         }
-    }
-
-    public boolean addNewCard(String name)
-        throws InterruptedException{
-        rfid.resetScanner();
-        rgb.ledOff();
-        lcd.writeText("Please scan a card for " + name);
-        rfid.writeToCard(name);
-        rgb.setColor(green);
-        names.put(name, false);
-        lcd.writeText("Write Successful");
-        Thread.sleep(2000);
-        lcd.clearDisplay();
-        rgb.ledOff();
-
-        return true;
+        else {
+            rgb.setColor(red);
+            return "Goodbye!";
+        }
     }
 }
